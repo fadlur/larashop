3d169eac3881f6820483e72d729b4f4d01129a80
Fadlur <fadloer@gmail.com>
2020-12-27 10:48:51 +0700

    template menggunakan bootstrap

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    public function index() {
+        $data = array('title' => 'Homepage');
+        return view('homepage.index', $data);
+    }
+}
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+  <div class="container">
+    <a class="navbar-brand" href="/">Toko</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="mr-auto navbar-nav"></ul>
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Produk</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kategori</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kontak</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tentang Kami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
