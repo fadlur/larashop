9946114c89739393a880f437d343150cd54fece7
Fadlur <fadloer@gmail.com>
2020-12-27 20:30:45 +0700

    setting role user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');//no tlp
+            $table->text('alamat')->nullable();//alamat
+            $table->string('role')->default('member');//member non admin
+            $table->string('foto')->nullable();//foto profil
+            $table->string('status')->default('aktif');//aktif atau non aktif, customer bisa kita nonaktifkan tanpa menghapus datanya
             $table->rememberToken();
             $table->timestamps();
         });
